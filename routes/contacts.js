e0b7577da6dbf3cd30eab2bfa10cc4f8b9ec34b1
AnshuJalan <anshujalan206@gmail.com>
2020-07-21 18:34:34 +0530

    Contacts get and new routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,19 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+
 // @route GET /api/contacts
 // @desc Get all contacts
 // @access Private
-router.get('/', (req, res) => {
-  res.send('Get Contacts!');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).json({ error: 'Server Error!' });
+  }
 });
 
 // @route POST /api/contacts
 // @desc Create new contact
 // @access Private
-router.post('/', (req, res) => {
-  res.send('New Contact!');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      body('name', 'Please enter a name!').exists(),
+      body('phone', 'Please enter a valid phone number!').isLength(10),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+
+    try {
+      const { name, phone, email, type } = req.body;
+
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      res.status(500).json({ error: 'SERVER ERROR!' });
+    }
+  }
+);
 
 // @route PUT /api/contacts
 // @desc Update contact
